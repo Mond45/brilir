9a63f3bf37388e38d9b74a748acd7efd1e30c7e3
Mond45 <mondc45@gmail.com>
2025-12-09 09:21:37 -0500

    add test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+INPUT_DIR="inputs"
+PROGRAM="build/bin/bril2mlir"
+OUTPUT_FILE="results.csv"
+
+: > "$OUTPUT_FILE"   # truncate output file
+
+for file in "$INPUT_DIR"/*; do
+    [ -f "$file" ] || continue
+
+    echo "Testing file: $file"
+
+    "$PROGRAM" < "$file" > /dev/null 2>&1
+    rc=$?
+
+    printf "%s,%d\n" "$(basename "$file")" "$rc" >> "$OUTPUT_FILE"
+done
